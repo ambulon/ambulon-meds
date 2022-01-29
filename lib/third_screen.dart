c949a2464f4d6f06d4b119c64905fb296d0cd40e
rifafatima <rifafatima02@gmail.com>
2022-01-29 12:32:38 +0530

    bottom nav bar

---
+++ lib/third_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class Third extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+    );
+  }
+}
